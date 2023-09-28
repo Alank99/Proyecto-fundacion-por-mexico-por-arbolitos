1bfd6a8a0a2eda848431c8b0aacc2c8b69d67deb
Mario Frias <a01782559@tec.mx>
2023-09-28 15:56:20 -0600

    Fixed dataprovider

--- frontend/src/dataProvider.jsx
+++ frontend/src/dataProvider.jsx
@@ -1,4 +1,13 @@
 import jsonServerProvider from "ra-data-json-server";
+import { fetchUtils } from "react-admin";
 import host from './const.js'
 
-export const dataProvider = jsonServerProvider(`http://${host}:1337`);
+const fetchJsonUtil = (url, options={})=>{
+    if(!options.headers){
+        options.headers=new Headers({Accept: "application/json"})
+    }
+    options.headers.set("Authentication", localStorage.getItem("auth"));
+    return fetchUtils.fetchJson(url, options);
+};
+
+export const dataProvider = jsonServerProvider(`http://${host}:1337`, fetchJsonUtil);
