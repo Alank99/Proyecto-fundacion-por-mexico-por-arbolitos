69f495abd330e0a366dae97d80aeeeab135e9389
Alank99 <93668758+Alank99@users.noreply.github.com>
2023-10-18 10:52:49 -0600

    Modifcacion del estados

--- frontend/src/TicketList.jsx
+++ frontend/src/TicketList.jsx
@@ -1,5 +1,5 @@
 import { Datagrid, List, ReferenceManyField, TextField, DateField,
-       ReferenceField, Edit, SimpleForm,
+       ReferenceField, Edit, SimpleForm, useEditController,
        TextInput, SearchInput, Create,SelectInput, 
        EditButton, TabbedShowLayout, Show, Tab, useGetRecordId, SimpleShowLayout } from 'react-admin';
 import {useState,useEffect} from 'react';
@@ -84,7 +84,7 @@ export const TicketShow = props => { //Muestra los datos de un ticket al hacer c
               <strong>Región:</strong> <TextField source="region" label="Región" />
             </div>
             <div style={{ marginBottom: '10px' }}>
-              <strong>Número de folio:</strong> <TextField source="Numerodefolio" label="Número de folio" />
+              <strong>Número de folio: </strong> <TextField source="Numerodefolio" label="Número de folio" />
             </div>
 
             </SimpleShowLayout>
@@ -113,45 +113,59 @@ export const TicketShow = props => { //Muestra los datos de un ticket al hacer c
   );
 };
 
-export const TicketEdit = () => (//Edita el estado de un ticket
-  <Edit>
-    <SimpleForm>
-      <div style={{ marginBottom: '10px' }}>
-      <strong>ID de ticket: </strong>  <TextField source="id" label="ID" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Usuario: </strong> <ReferenceField source="id_cor" reference="usuarios" label="Usuario" link="show" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Id del usuario: </strong><TextField source="id_cor" label="Coordinador" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Prioridad: </strong><TextField source="prioridad" label="Prioridad" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Categoria: </strong><TextField source="categoria" label="Categoría" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Subcategoria: </strong><TextField source="subcategoria" label="Subcategoría" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Descripcion: </strong><TextField source="descripcion" label="Descripción" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Aula: </strong><TextField source="aula" label="Aula" />
-      </div>
-      <div style={{ marginBottom: '10px' }}> {/*Solo se puede editar el estado del ticket*/}
-        <SelectInput source="status" choices={status} label="Estado" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Fecha de creación: </strong><DateField source="fechaCreacion" label="Fecha de creación" />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        <strong>Región: </strong><TextField source="region" label="Región" />
-      </div>
-    </SimpleForm>
-  </Edit>
-);
+export const TicketEdit = (props) => {//Edita el estado de un ticket
+  const {record} = useEditController(props);
+  const isResolved = record.status === "Resuelto"; //verifica si el ticket esta resuelto
+  if (!record) {
+    return null; // No renderizar nada si el record es undefined o null
+  }
+
+  return(
+    <Edit {...props}>
+      <SimpleForm>
+        <div style={{ marginBottom: '10px' }}>
+        <strong>ID de ticket: </strong>  <TextField source="id" label="ID" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Usuario: </strong> <ReferenceField source="id_cor" reference="usuarios" label="Usuario" link="show" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Id del usuario: </strong><TextField source="id_cor" label="Coordinador" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Prioridad: </strong><TextField source="prioridad" label="Prioridad" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Categoria: </strong><TextField source="categoria" label="Categoría" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Subcategoria: </strong><TextField source="subcategoria" label="Subcategoría" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Descripcion: </strong><TextField source="descripcion" label="Descripción" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Aula: </strong><TextField source="aula" label="Aula" />
+        </div>
+        <div style={{ marginBottom: '10px' }}> {/*Solo se puede editar el estado del ticket*/}
+        {isResolved ? (
+          <div>
+          <strong>Estado: </strong><TextField source="status" label="Estado" />
+          </div>
+        ) : (
+          <SelectInput source="status" choices={status} label="Estado" />
+        )}
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Fecha de creación: </strong><DateField source="fechaCreacion" label="Fecha de creación" />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <strong>Región: </strong><TextField source="region" label="Región" />
+        </div>
+      </SimpleForm>
+    </Edit>
+  );
+};
 
 export const TicketCreate = () => {//Crea un ticket nuevo
   const [categoriaSec, setCategoriasSec] = useState("");
@@ -191,6 +205,7 @@ export const TicketCreate = () => {//Crea un ticket nuevo
           <SelectInput
             source="subcategoria"
             choices={[subcategoriaDefaultOption, ...subcategoriaSec.map((subcat) => ({ id: subcat.id, name: subcat.name }))]}
+            label="Subcategoría"
           />
         )}
         <div>
@@ -203,10 +218,10 @@ export const TicketCreate = () => {//Crea un ticket nuevo
         </div>
 
         {hasFolio && (
-          <TextInput source="Numerodefolio"  />//verifica que si el checkbox esta marcado despliega el folio
+          <TextInput source="Numerodefolio" label = "Numero de folio" />//verifica que si el checkbox esta marcado despliega el folio
         )}
-        <TextInput multiline rows={5} fullWidth source="descripcion" />
-        <TextInput source="aula" />
+        <TextInput multiline rows={5} fullWidth source = "Descripcion" />
+        <TextInput source="aula" label = "Aula"/>
       </SimpleForm>
     </Create>
   );
